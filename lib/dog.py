1eb66862013a79c27986e0f536e5ae1575965b7d
Christina Kozanian <christinakozanian@gmail.com>
2024-01-04 17:47:58 -0800

    lab complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,13 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    
+    def __init__(self, name='Bruce', breed='Beagle'):
+        if type(name) == str and 1 <= len(name) <= 25:
+            self.name = name
+            if type(breed) == str and breed in APPROVED_BREEDS:
+                self.breed = breed
+            else:
+                print('Breed must be in list of approved breeds.')
+        else: 
+            print('Name must be string between 1 and 25 characters.')
